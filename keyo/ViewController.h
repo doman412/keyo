5f51c2960ecaae72dbfdba641fed000807f459ee
Derek Arner <doman412@gmail.com>
2013-12-22 17:46:24 -0500

    Initial Commit

---
+++ keyo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  keyo
+//
+//  Created by Derek Arner on 12/22/13.
+//  Copyright (c) 2013 MakoWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ keyo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  keyo
+//
+//  Created by Derek Arner on 12/22/13.
+//  Copyright (c) 2013 MakoWare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
